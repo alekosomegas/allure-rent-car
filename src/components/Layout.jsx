0778fe1daec78e132db0db6260d8996702529b5b
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-06-06 16:01:19 +0300

    moved scripts to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import Script from 'next/script'
 
 export default function Layout({
     children,
